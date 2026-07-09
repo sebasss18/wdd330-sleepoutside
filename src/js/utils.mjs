af3cd0cc7505bbcbac1e7a4147f93331b757f7bc
sebasss <sebasbernals11@gmail.com>
2026-07-09 09:32:40 -0700

    Revert "Completed dynamic product list"

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,12 +9,10 @@ export function qs(selector, parent = document) {
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -24,20 +22,8 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-// --- NEW FUNCTIONS BELOW ---
-
-// Retrieve a parameter from the URL 
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
-}
-
-// Render a list of items using a template function 
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-  const htmlStrings = list.map(templateFn);
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
