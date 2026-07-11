b177db403e5c7e1816d606c2e6d92e29e16f7b1c
marcelo-sduarte <marcelo.sduarte@hotmail.com>
2026-07-11 09:49:13 +0100

    ajust flash and quick view

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,19 +1,17 @@
 import { renderListWithTemplate } from "./utils.mjs";
 import QuickView from "./QuickView.mjs";
 
+const allowedIds = ["880RR", "985RF", "985PR", "344YJ"];
+
 function productCardTemplate(product) {
   return `<li class="product-card">
-    <a href="product_pages/?product=${product.Id}">
-      <img src="${product.Image.replace("../", "/")}" alt="Image of ${product.Name}">
-      <h2 class="card__brand">${product.Brand.Name}</h2>
-      <h3 class="card__name">${product.NameWithoutBrand}</h3>
+    <a href="/product_pages/?product=${product.Id}">
+      <img src="/images/tents/${product.Image.split("/").pop()}" alt="Image of ${product.NameWithoutBrand}">
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
       <p class="product-card__price">$${product.FinalPrice}</p>
     </a>
-      <button
-        class="quick-view-btn"
-        data-id="${product.Id}">
-        Quick View
-      </button>
+    <button class="quick-view-btn" data-id="${product.Id}">Quick View</button>
   </li>`;
 }
 
@@ -22,15 +20,26 @@ export default class ProductList {
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
-
+    this.quickView = new QuickView(dataSource);
   }
-  
+
   async init() {
     const list = await this.dataSource.getData();
-    this.renderList(list);
+    const filteredList = list.filter((product) => allowedIds.includes(product.Id));
+    this.renderList(filteredList);
   }
-  
+
   renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list);
+    this.addQuickViewEvents();
+  }
+
+  addQuickViewEvents() {
+    const buttons = document.querySelectorAll(".quick-view-btn");
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        this.quickView.show(button.dataset.id);
+      });
+    });
   }
 }
