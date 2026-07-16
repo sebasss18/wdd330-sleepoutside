7962b2d5512ec7b8e0d67b75eb15fd1cf3635cab
sebasss18 <sebasbernals11@gmail.com>
2026-07-16 08:30:52 -0700

    Refactor Shopping Cart

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,57 @@
+import { getLocalStorage } from "./utils.mjs";
+
+function cartItemTemplate(item) {
+  return `<li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
+}
+
+export default class ShoppingCart {
+  constructor(listElement) {
+    this.listElement = listElement;
+  }
+
+  renderCartContents() {
+    const cartItems = getLocalStorage("so-cart");
+
+    const items = Array.isArray(cartItems)
+      ? cartItems
+      : cartItems
+        ? [cartItems]
+        : [];
+
+    const cartFooter = document.querySelector(".cart-footer");
+
+    if (items.length > 0) {
+      const htmlItems = items.map(cartItemTemplate);
+      this.listElement.innerHTML = htmlItems.join("");
+
+      cartFooter.classList.remove("hide");
+
+      const total = items.reduce((sum, item) => sum + item.FinalPrice, 0);
+
+      document.querySelector(".cart-total").innerHTML =
+        `Total: $${total.toFixed(2)}`;
+    } else {
+      this.listElement.innerHTML =
+        "<p>Your cart is currently empty. Go find some gear!</p>";
+
+      cartFooter.classList.add("hide");
+    }
+  }
+
+  init() {
+    this.renderCartContents();
+  }
+}
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,54 +1,9 @@
-import { getLocalStorage, updateCartCount, loadHeaderFooter } from "./utils.mjs";
-
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  // This safely handles if the cart is an array, a single object, or completely empty
-  const items = Array.isArray(cartItems)
-    ? cartItems
-    : cartItems
-      ? [cartItems]
-      : [];
-
-  // Check if there are actually items to display
-  if (items.length > 0) {
-    const htmlItems = items.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
-    // 1. Show the footer
-    const cartFooter = document.querySelector(".cart-footer");
-    cartFooter.classList.remove("hide");
-
-    // 2. Calculate the total
-    const total = items.reduce((sum, item) => sum + item.FinalPrice, 0);
-
-    // 3. Display the total
-    const totalElement = document.querySelector(".cart-total");
-    totalElement.innerHTML = `Total: $${total.toFixed(2)}`;
-  } else {
-    // If empty, show a nice message instead of a blank space
-    document.querySelector(".product-list").innerHTML = "<p>Your cart is currently empty. Go find some gear!</p>";
-    cartFooter.classList.add("hide");
-  }
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
+import { loadHeaderFooter, updateCartCount } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
-renderCartContents();
+
+const cart = new ShoppingCart(document.querySelector(".product-list"));
+cart.init();
+
 updateCartCount();
